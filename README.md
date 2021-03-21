8c00bdab3bdcc39c3f28b22ac4cb675a9888a2f0
MetricT <MetricT@like.naw.dawg>
2021-03-20 23:17:52 -0500

    * Reupload since I borked the repo somehow

--- README.md
+++ README.md
@@ -1,2 +1,222 @@
-# TN_Campaign_Finance
-TN State Legislature campaign finance data
+# Tennessee Campaign Finance Data
+
+The Tennessee State Legislature has a public Campaign Finance website, in theory
+to ensure transparency in our elected officials.   In practice, the website was
+deliberately crippled to make it tedious and difficult to use, in order to deter
+users from actually using it for its intended purpose.
+
+I've been dealing with this for years, and got fed up with it a couple months
+ago.   Since it's public data and I have zero faith that the TN Legislature will
+voluntarily improve their transparency, I decided to do it myself by downloading
+the data on the "Contributions and Expenditures" page one spreadsheet at a time,
+organizing, and merging them together.  This required downloading, stitching 
+together, sanity-checking, and cleaning over 500 spreadsheets of data.  That was
+absolutely zero fun whatsoever, do not recommend at all...
+
+Now you can use modern data analysis tools like R, Tableau, Excel, SQL, or even
+"awk | grep | sed" (which is still better than the campaign finance website) to
+analyze the entire 20 year dataset from 2000-2020 and see which PACs, business,
+elected officials, and individuals are donating to politicians and PACs.  It's 
+not perfect, but it's the best we're likely to get until there's a regime change
+or someone on the inside dumps the existing database and posts it on Pastebin.
+
+![Contributions to Candidates by Source](https://i.imgur.com/1KVjeBj.png)
+
+There are two different versions of the TN Campaign Finance data available here:
+
+* A "pristine" version.   Take the reports for each year and concatenate them
+together.   Added a "Source" = {Candidate, PAC, Business, Individual} and 
+"Dest" = {Candidate, Pac} field for Contributions to correspond to the 
+equivalent selectors on the website, and a "Source" = {Candidate, PAC, Other}
+and "Dest" = {Candidate, Pac} field for Expenditures.   Barring gross error, 
+this version is fairly static, though the current and previous year will require
+occasional updates as new or late reports are released.
+
+* The "cleaned" version uses the "pristine" version as a base.   I merged all 
+years into a single database for {Contributions, Expenditures}, converted two 
+different date formats and a bunch of dates with typos into ISO date format to
+make searches simpler, converted dollar amounts from a character string to a 
+numeric to make analysis easier, and added a "Report.Year" variable that 
+corresponds to the reporting year.
+
+# Decompressing the data
+
+To save bandwidth, all files are compressed using the xz compressor.   On Linux
+(and presumably Mac, sorry I don't have access to one), just run `xz -d *.xz`
+to decompress the files.   On Windows, use [7-Zip](www.7-zip.org) (FREE) to 
+decompress.
+
+# Using the data
+
+The files themselves are .csv spreadsheets and can be viewed by any spreadsheet
+such as:
+
+   - [LibreOffice Calc](https://www.libreoffice.org/) (FREE)
+   - [Microsoft Excel](https://www.microsoft.com/en-us/microsoft-365/excel)
+   - [Google Sheets](https://www.google.com/sheets/about/)
+   - [Apple iWork Numbers](https://www.apple.com/iwork/)
+   
+For complex analysis you probably want to use dedicated analysis software like:
+
+   - [RStudio](https://rstudio.com/) (FREE, and what I used)
+   - [Tableau Public Edition](https://public.tableau.com/en-us/s/download) (FREE)
+   - [Tableau Commercial Version](https://www.tableau.com)
+   - [Microsoft Power BI](https://powerbi.microsoft.com/en-us/)
+   - [Stata](https://www.stata.com/)
+   
+In addition, .csv files can be imported into popular databases (SQLite, MySQL, 
+PostgreSQL, etc), and then accessed programmatically from your preferred 
+language (Python, Perl, Java, C#, etc.)
+
+# How you can help:
+
+* If anyone with website skills wants to put up a public dashboard, I think it
+would be quite useful for the public to be able to see who is funding their
+elected officials, and who is funding the funders.   Which PACs/Businesses
+donate the most money, which candidates receive the most money, and does that
+affect the outcome of votes?
+
+* Create ancillary datasets to help bolster the usefulness of this data.   You 
+don't have to do them all, but by doing a couple of entries you can help 
+contribute.  In particular, it would be useful to have:
+
+   - A spreadsheet for candidates with their party affiliation, office sought, 
+     election year, district, PAC if they have one
+     
+   - A spreadsheet for PACs/businesses with info on their Industry (alcohol, 
+     payday loans, real estate, etc), to make it easier to filter contributions
+     or expenditures by industry, to see if PAC contribution is affecting 
+     legislative outcomes.  
+
+
+* Mine the data and ask questions.  A few that have occurred to me so far:
+
+   - Look at expenditures and see if candidates are using their campaign money
+     for questionable expenses, like "GOLF TOURNAMENT", "BALLOON ARTIST", 
+     "AR-15 RIFLES FOR HOGFEST GIVEAWAY", "BBQ PIT PAINT JOB" (yes, those are 
+     actual items).
+     
+   - Do some double-entry accounting to verify that the donations PAC's post on 
+     their expenses are matched by contributions reported by the Candidate.
+     
+   - Look for candidates or donors laundering money by circulating them around
+     PACs.
+     
+   - See if politicians who receive a larger share of their income from 
+     PACs/businesses tend to ignore their constituent's wishes.    
+
+There are many interesting questions both journalistic and academic whose 
+answers lie within.
+
+* Help pierce the corporate veil of PACs.   It used to be that a individual or
+business gave money directly to a candidate.    Now, in order to evade campaign
+finance restrictions, money often goes individual -> PAC -> candidate, or even
+more complex loops (individual -> PAC_1 -> PAC_2 -> PAC_3 -> Candidate).   The
+database has info on individuals/businesses that donate to PACs, PACs that 
+donate to other PACs, and PACs that donate to candidates.   Use the data to cut
+through the Gordian Knot of PACs so we can get a clearer view on who is donating
+to our candidates.
+
+* Help verify the data.   I have done the best I can to check the data, but
+there's only so much I can do given how torturous it was to assemble in the
+first place.  I have released sha256sums of the downloaded datasets.   The datasets
+are labeled 
+`{cont,exp}_{year}_{candidate,pac}_{candidate,pac,individual,business,other}_{index}`.  
+If you go to the "Contributions and Expenditures" site, select Contribution or 
+Expenditures `{cont,exp}`, then the year `{year}`, then whether next selector is 
+candidate or pac `{candidate,pac}`, then whether next selector is 
+`{candidate,pac,individual,business,other}`, then an index based on page number 
+(some have multiple pages so you have to click "MORE" at the bottom to go to the
+next page), and always select all the "Display these fields in my results" boxes
+at the bottom.   Download a few random ones, take a sha256sum, and compare it to 
+the list in Github.   If there is a difference, let me know and I'll fix it.
+
+# FAQ
+
+* **Why did you hack the state government?**
+
+I didn't "hack" the state government.   It's public data and I accessed it
+using the same horrible 
+[TN Campaign Finance webform](https://apps.tn.gov/tncamp-app/public/search.htm) 
+that everyone else uses.  When you do a search, they have an option to "Save as 
+Excel" at the bottom.  So I did.  Many, many times.  They make the website
+resistant to automating even that part, so I had to download everything by hand
+over a couple of months.  Then I reassembled those Excel spreadsheets back into 
+something as closely resembling the original database as possible.
+
+* **Why did you release this data?**
+
+The TN State Legislature makes the Campaign Finance report website as difficult
+to use as they can get away with.  It is tedious, takes a long time to get 
+anywhere, and only offers the data in bite-sized morsels that make it resistant
+to easy and/or deep analysis.  I would have greatly preferred if TN had offered 
+the public data in a more useful form, but after a decade of people begging them
+and nothing happening, it became glaringly obvious that they would not improve
+their transparency willingly.
+
+* **How do I use the data?**
+
+The data is simple spreadsheets.  Any spreadsheet (Excel, iWork, OpenOffice, 
+LibreOffice) should be able to easily view the files.  But for complex querying
+you probably want to use data analysis software like:
+
+   - [RStudio](https://rstudio.com/) (FREE)
+   - [Tableau Public Edition](https://public.tableau.com/en-us/s/download) (FREE)
+   - [Tableau Commercial Version](https://www.tableau.com)
+   - [Microsoft Power BI](https://powerbi.microsoft.com/en-us/)
+   - [Stata](https://www.stata.com/)
+   
+Or you may prefer to import it into a SQL database.   It's bog-standard .csv
+and should easily import into SQLite, MySQL, Postgresql, Oracle, and SQL server. 
+Once there you should be able to access it using your preferred language
+(Python, Perl, Visual Basic, Java, etc.)
+
+* **I think I've found a mistake or missing data!**
+
+Please open a bug report about it, and I'll fix the data as soon as I can.  Give
+me as much info as possible. 
+
+* **What's Next?**
+
+I will likely make further revisions to the "cleaned" version, like add a field
+for the Legislative Session (112, 110, etc), tweaking Contributor/Recipient
+names to make sure they are consistent and have no typos, or splitting the
+"Address" fields into "Street", "City", "ZIP" to make searches easier, though I
+will leave the original fields intact in case anyone builds apps or workflow
+around them.   A lot of these fields have typos of various sorts, and I'm trying
+to figure out a way to clean them up.
+
+I also intend to keep this dataset updated whenever new reports are released, as
+long as I can do so.
+
+* **I'd like to contribute...**
+
+By all means!   If you write an interesting bit of code or a cool graph, please
+submit it and I'll add it up here.
+
+* **I sent you a message and didn't hear back**
+
+I only check Github infrequently, so if you sent me something and haven't heard
+back from me in a reasonable time, just PM /u/MetricT on Reddit.
+
+* **You're so awesome!!  How can I repay you?**
+
+Just send me a message telling me how you used the data.  It's always nice to 
+hear back from folks, plus you might give me good ideas.
+
+As for something more material, thanks but I'm good.  If you have money burning
+a hole in your pocket, donate it to the
+[Society of Professional Journalists](https://www.spj.org/donate.asp)
+and help support the folks bringing us the news everyday.  
+
+# Thanks...
+
+My thanks to Phil Williams, Tom Humphrey, Erik Schelzig, Natalie Allison, 
+Chas Sisk, Sergio Martinez-Beltran, Cari Wade Gervin, Betsy Phillips, and the 
+many others local journalists who work long hours and endure endless abuse to 
+bring us the news.   
+
+People don't go into reporting for the big bucks.  They do it for passion 
+and to make a difference.  And you all do.
+
+Luke 8:17
---
+++ code/R/TN_Campaign_Finance_examples.R
@@ -0,0 +1,429 @@
+################################################################################
+### Some examples of how to use the Campaign Finance DB in R
+################################################################################
+library(plyr)
+library(tidyverse)
+library(lubridate)
+library(gt)
+
+### Load the Campaign Finance data
+contrib <- read_csv("../../Contributions_cleaned.csv")
+expend  <- read_csv("../../Expenditures_cleaned.csv")
+pacs    <- read_csv("../../PACS_cleaned.csv") %>% select(-Salutation, -First.Name)
+
+### Load a file containing lists of various industry PACs, Business, and Individuals
+source("pac_business_individuals.R")
+
+################################################################################
+### Example 1:  Graph contributions by Individuals, Candidates, Busineses, 
+###             and PACs to Candidates by Year/Month
+################################################################################
+
+data <- 
+  contrib %>% 
+  filter(Dest == "Candidate") %>%
+  select(Date, Amount, Source) %>% 
+  mutate(Year = year(Date), Month = month(Date)) %>% 
+  select(-Date) %>% 
+  group_by(Year, Month, Source) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup %>% 
+  mutate(Date = as.Date(paste(Year, "-", Month, "-01", sep = ""))) %>% 
+  select(Date, Amount, Source) %>% 
+  filter(!is.na(Date))
+
+# Optional:  Normalize Amount using the max value for each source.   This helps
+#            prevent a flood of contributions by PACs from making patterns in
+#            contributions from Individuals too faint to see.
+#
+#data <-
+#  data %>% 
+#  left_join((data %>% 
+#       select(Source, Amount) %>% 
+#       group_by(Source) %>%  
+#       summarize(MaxAmount = max(Amount)) %>% 
+#       ungroup()), by = "Source") %>% 
+#  mutate(Amount = Amount / MaxAmount)
+
+# Graph the result
+g_campaign_contributions <-
+  
+  ggplot(data = data, aes(x = year(Date), y = month(Date), fill = Amount)) +
+
+  theme_minimal() +
+  theme(
+      legend.position = "none",
+      plot.title = element_text(hjust = 0.5),
+      plot.subtitle = element_text(hjust = 0.5),
+      plot.caption = element_text(hjust = 0.5)) +
+
+  geom_tile(color = "white") + 
+  
+  labs(title = "Contributions to TN Legislators by Year, Month", x = "", y = "") +
+  
+  scale_fill_gradient(low = "white", high = "darkred") +
+  
+  scale_y_continuous(trans = "reverse",
+     breaks = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+     labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")) + 
+  
+  scale_x_continuous(
+    breaks = c(2000, 2001, 2003, 2005, 2007, 2009, 
+               2011, 2013, 2015, 2017, 2019, 2021), 
+     limits = c(2003, 2021),
+     labels = c("101st\n2000", "102nd\n2001", "103rd\n2003", "104th\n2005", 
+                "105th\n2007", "106th\n2009", "107th\n2011", "108th\n2013",
+                "109th\n2015", "110th\n2017", "111st\n2019", "112nd\n2021")) +
+  
+  facet_wrap(~ Source, nrow = 2, ncol = 2, scales = "free")
+
+print(g_campaign_contributions)
+
+################################################################################
+### Example 2:  Find the top 20 Individual donors to Candidates since 2018
+################################################################################
+
+contrib %>%
+  filter(Source == "Individual") %>%
+  filter(Dest   == "Candidate") %>% 
+  filter(Report.Year >= 2018) %>%
+  select(Amount, Contributor.Name) %>% 
+  group_by(Contributor.Name) %>% 
+  summarize(Amount = sum(Amount)) %>% ungroup() %>% 
+  arrange(desc(Amount)) %>% 
+  print(n = 20)
+
+################################################################################
+### Example 3:  Find the top 20 PACS outside TN donating to Candidates
+################################################################################
+
+outside_tn_pacs <- pacs %>% filter(State != "TN") %>% pull(Last.Name)
+
+contrib %>% 
+  filter(Source == "PAC" & Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% outside_tn_pacs) %>%
+  select(Contributor.Name, Amount) %>%
+  group_by(Contributor.Name) %>%
+  summarize(Amount = sum(Amount)) %>%
+  ungroup() %>%
+  arrange(desc(Amount)) %>%
+  print(n = 20)
+
+################################################################################
+### Example 4:  Top 20 Candidates receiving donations from other legislators 
+###             since 2016
+################################################################################  
+
+contrib %>% 
+  filter(Source == "Candidate" & Dest == "Candidate") %>% 
+  filter(Report.Year <= 2016) %>%
+  filter(!is.na(Amount)) %>% 
+  select(Amount, Recipient.Name) %>% 
+  group_by(Recipient.Name) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(desc(Amount)) %>% 
+  
+  # Optional:  Filter out gubernatorial candidates so we can just look at the legislature
+  filter(!grepl("BOYD, RANDY|BLACK, DIANE|DEAN, KARL|LEE, BILL|HASLAM, BILL|GOVERNOR", Recipient.Name)) %>%
+
+  print(n = 20)
+
+################################################################################
+### Example 5:  What was the effect of "Citizens United" on the number of PACs
+###             and dollar volume of PAC contributions?
+################################################################################
+
+### Graph the number of PACs using the info in the PAC database
+g_number_pacs <-
+  pacs %>% 
+  select(Created) %>% 
+  mutate(Year = year(Created)) %>% 
+  select(Year) %>% 
+  filter(Year != 1990) %>% 
+  filter(Year <= 2020) %>% 
+  filter(Year >= 2004) %>% # Because data before 2004 is buggy and unreliable
+  
+  mutate(Session = case_when(
+    Year == 2004   ~ 103,
+    Year == 2005   ~ 104,
+    Year == 2006   ~ 104,
+    Year == 2007   ~ 105,
+    Year == 2008   ~ 105,
+    Year == 2009   ~ 106,
+    Year == 2010   ~ 106,
+    Year == 2011   ~ 107,
+    Year == 2012   ~ 107,
+    Year == 2013   ~ 108,
+    Year == 2014   ~ 108,
+    Year == 2015   ~ 109,
+    Year == 2016   ~ 109,
+    Year == 2017   ~ 110,
+    Year == 2018   ~ 110,
+    Year == 2019   ~ 111,
+    Year == 2020   ~ 111,
+    Year == 2021   ~ 112,
+  )) %>%
+  
+  group_by(Session) %>% 
+  summarize(n = n()) %>% 
+  ungroup() %>% 
+  
+  mutate(Year = case_when(
+    Session == 103  ~ 2003,
+    Session == 104  ~ 2005,
+    Session == 105  ~ 2007,
+    Session == 106  ~ 2009,
+    Session == 107  ~ 2011,
+    Session == 108  ~ 2013,
+    Session == 109  ~ 2015,
+    Session == 110  ~ 2017,
+    Session == 111  ~ 2019,
+    Session == 112  ~ 2021,
+  )) %>%
+  
+  arrange(Year) %>% 
+  ggplot() + 
+  theme_bw() +
+  geom_bar(stat = "identity", aes(x = Year, y = n), fill = "darkseagreen4") +
+  geom_vline(xintercept = decimal_date(as.Date("2010-01-21")), linetype = "dashed") +
+  scale_x_continuous(
+    breaks = c(2000, 2001, 2003, 2005, 2007, 2009, 
+               2011, 2013, 2015, 2017, 2019, 2021), 
+    labels = c("101st\n2000", "102nd\n2001", "103rd\n2003", "104th\n2005", 
+               "105th\n2007", "106th\n2009", "107th\n2011", "108th\n2013",
+               "109th\n2015", "110th\n2017", "111st\n2019", "112nd\n2021")) +
+  labs(x = "", y = "",
+       title = "Number of New PACs in TN by year created",
+       subtitle = "Dashed line shows date of US Supreme Court Decision \"Citizens United v FEC\"")
+
+g_dollar_pacs <- 
+  contrib %>% 
+  filter(Source == "PAC") %>% 
+  filter(Report.Year >= 2004) %>% 
+  select(Report.Year, Amount) %>% 
+  group_by(Report.Year) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  mutate(Session = case_when(
+    Report.Year == 2004   ~ 103,
+    Report.Year == 2005   ~ 104,
+    Report.Year == 2006   ~ 104,
+    Report.Year == 2007   ~ 105,
+    Report.Year == 2008   ~ 105,
+    Report.Year == 2009   ~ 106,
+    Report.Year == 2010   ~ 106,
+    Report.Year == 2011   ~ 107,
+    Report.Year == 2012   ~ 107,
+    Report.Year == 2013   ~ 108,
+    Report.Year == 2014   ~ 108,
+    Report.Year == 2015   ~ 109,
+    Report.Year == 2016   ~ 109,
+    Report.Year == 2017   ~ 110,
+    Report.Year == 2018   ~ 110,
+    Report.Year == 2019   ~ 111,
+    Report.Year == 2020   ~ 111,
+    Report.Year == 2021   ~ 112,
+  )) %>%
+  group_by(Session) %>%
+  summarize(Amount = sum(Amount)) %>%
+  ungroup() %>%
+  mutate(Report.Year = case_when(
+    Session == 103  ~ 2003,
+    Session == 104  ~ 2005,
+    Session == 105  ~ 2007,
+    Session == 106  ~ 2009,
+    Session == 107  ~ 2011,
+    Session == 108  ~ 2013,
+    Session == 109  ~ 2015,
+    Session == 110  ~ 2017,
+    Session == 111  ~ 2019,
+    Session == 112  ~ 2021,
+  )) %>%
+  ggplot() + 
+  theme_bw() + 
+  geom_bar(stat = "identity", aes(x = Report.Year, y = Amount), fill = "darkred") +
+  scale_y_continuous(labels = scales::dollar) +
+  geom_vline(xintercept = decimal_date(as.Date("2010-01-21")), linetype = "dashed") +
+  scale_x_continuous(
+    breaks = c(2000, 2001, 2003, 2005, 2007, 2009, 
+               2011, 2013, 2015, 2017, 2019, 2021), 
+    labels = c("101st\n2000", "102nd\n2001", "103rd\n2003", "104th\n2005", 
+               "105th\n2007", "106th\n2009", "107th\n2011", "108th\n2013",
+               "109th\n2015", "110th\n2017", "111st\n2019", "112nd\n2021")) +
+  labs(x = "", y = "", 
+       title = "Dollars Donated in PACs by Legislative Session",
+       subtitle = "Dashed line shows date of US Supreme Court Decision \"Citizens United v FEC\"")
+
+print(plot_grid(g_number_pacs, g_dollar_pacs, nrow = 1, ncol = 2, align = "hv"))
+
+
+################################################################################
+### Example 6:  How did Citizens United affect PAC contributions from various
+###             industries?
+################################################################################
+
+### Large ISP's (AT&T/Bellsouth, Verizon, Charter, Comcast)
+g_isp <- 
+  contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% pac_isp) %>% 
+  filter(Report.Year >= 2004) %>% 
+  select(Report.Year, Amount) %>% 
+  group_by(Report.Year) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ggplot() + 
+  theme_bw() + 
+  geom_bar(stat = "identity", fill = "darkseagreen4",
+           aes(x = Report.Year, y = Amount)) + 
+  scale_y_continuous(labels = scales::dollar) + 
+  geom_vline(xintercept = 2010, linetype = "dashed", color = "steelblue2") + 
+  labs(x = "", 
+       title = "Contributions from ISP's (AT&T/Verizon/Comcast/Charter) to TN Candidates", 
+       subtitle = "Dashed line represents date US Supreme Court decided Citizens United v F.E.C.")
+#print(g_isp)
+
+
+# Alcohol manufacturers, wholesalers, distributors, and shops
+g_alcohol <- 
+contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% pac_alcohol) %>% 
+  select(Report.Year, Amount) %>% 
+  group_by(Report.Year) %>% 
+  filter(Report.Year >= 2004) %>%
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(Report.Year) %>%
+  ggplot() + 
+  theme_bw() + 
+  geom_bar(stat = "identity", fill = "darkseagreen4",
+           aes(x = Report.Year, y = Amount)) + 
+  scale_y_continuous(labels = scales::dollar) + 
+  geom_vline(xintercept = 2010, linetype = "dashed", color = "steelblue2") + 
+  labs(x = "", title = "Contributions from Alcohol Industry to TN Candidates", 
+       subtitle = "Dashed line represents date US Supreme Court decided Citizens United v F.E.C.")
+#print(g_alcohol)
+
+print(plot_grid(g_isp, g_alcohol, nrow = 1, ncol = 2, align = "hv"))
+
+
+################################################################################
+### Example 7:  Compare contributions from cannabis and several antagonistic 
+###             businesses and organizations
+################################################################################
+
+data_alcohol <- 
+  contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% pac_alcohol) %>% 
+  filter(Report.Year >= 2004) %>%
+  select(Report.Year, Amount) %>% 
+  group_by(Report.Year) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(Report.Year) %>%
+  rename(Alcohol = Amount)
+
+data_private_prisons <- 
+  contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% pac_private_prisons) %>% 
+  filter(Report.Year >= 2004) %>%
+  select(Report.Year, Amount) %>% 
+  group_by(Report.Year) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(Report.Year) %>%
+  rename(`Private Prisons` = Amount)
+
+data_pharma <-
+  contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% pac_pharma_sales) %>% 
+  filter(Report.Year >= 2004) %>%
+  select(Report.Year, Amount) %>% 
+  group_by(Report.Year) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(Report.Year) %>%
+  rename(`Pharma`= Amount)
+
+data_cannabis <- 
+  contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% c(pac_cannabis, business_cannabis)) %>% 
+  filter(Report.Year >= 2004) %>%
+  select(Report.Year, Amount) %>% 
+  group_by(Report.Year) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(Report.Year) %>%
+  rename(Cannabis = Amount)
+
+data <-
+  data_alcohol %>%
+  full_join(data_private_prisons, by = "Report.Year") %>% 
+  full_join(data_cannabis,        by = "Report.Year") %>%
+  full_join(data_pharma,          by = "Report.Year") %>%
+  pivot_longer(-Report.Year,
+               names_to = "Industry",
+               values_to = "Amount") %>% 
+  mutate(Industry = fct_relevel(Industry,
+                                c("Alcohol", 
+                                  "Private Prisons",
+                                  "Pharma", 
+                                  "Cannabis")))
+
+g_mmj_and_foes <-
+  ggplot(data = data) + 
+  theme_bw() + 
+  geom_bar(stat = "identity", fill = "darkseagreen4",
+           aes(x = Report.Year, y = Amount)) + 
+  scale_y_continuous(labels = scales::dollar) + 
+  labs(x = "", title = "Contributions from Cannabis and Opponents to TN Candidates") +
+  facet_wrap(~ Industry, nrow = 2, ncol = 2)
+
+print(g_mmj_and_foes)
+
+
+################################################################################
+### Example 8:  Where do Alcohol industry contributions come from?
+################################################################################
+
+# Top PAC contributors
+contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% pacs_alcohol) %>% 
+  filter(Report.Year >= 2004) %>%
+  select(Contributor.Name, Amount) %>% 
+  group_by(Contributor.Name) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(desc(Amount)) %>% 
+  print(n = 20)
+
+# Top business contributors
+contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% business_alcohol) %>% 
+  filter(Report.Year >= 2004) %>%
+  select(Contributor.Name, Amount) %>% 
+  group_by(Contributor.Name) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(desc(Amount)) %>% 
+  print(n = 20)
+
+# Top Individual contributors
+contrib %>% 
+  filter(Dest == "Candidate") %>% 
+  filter(Contributor.Name %in% individual_alcohol) %>% 
+  filter(Report.Year >= 2004) %>%
+  select(Contributor.Name, Amount) %>% 
+  group_by(Contributor.Name) %>% 
+  summarize(Amount = sum(Amount)) %>% 
+  ungroup() %>% 
+  arrange(desc(Amount)) %>% 
+  print(n = 20)
